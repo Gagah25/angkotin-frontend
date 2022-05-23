5156c6a9904fe166905c60575610999983545f77
Gagah25 <gagahphs@gmail.com>
2022-05-23 18:56:33 +0700

    Memperbaiki layout maps lokasi

--- app/src/main/java/com/example/angkotin/HomeActivity.kt
+++ app/src/main/java/com/example/angkotin/HomeActivity.kt
@@ -1,11 +1,40 @@
 package com.example.angkotin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.angkotin.databinding.ActivityHomeBinding
+import com.example.angkotin.ui.BantuanActivity
+import com.example.angkotin.ui.MapsActivity
+import com.example.angkotin.ui.RuteAngkotActivity
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnCariAngkot.setOnClickListener{ moveToCariAngkot() }
+            btnRuteAngkot.setOnClickListener { moveToRuteAngkot() }
+            btnBantuan.setOnClickListener { moveToBantuan() }
+        }
+    }
+
+    private fun moveToCariAngkot(){
+        val intent = Intent(this@HomeActivity, MapsActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun moveToRuteAngkot(){
+        val intent = Intent(this@HomeActivity, RuteAngkotActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun moveToBantuan(){
+        val intent = Intent(this@HomeActivity, BantuanActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/angkotin/ui/MapsActivity.kt
+++ app/src/main/java/com/example/angkotin/ui/MapsActivity.kt
@@ -4,27 +4,32 @@ import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.provider.Settings
+import android.view.MenuItem
+import android.view.View
+import android.widget.RelativeLayout
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import com.example.angkotin.HomeActivity
 import com.example.angkotin.R
 import com.example.angkotin.databinding.MapsLokasiBinding
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapsInitializer
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.material.navigation.NavigationView
 
-class MapsActivity: AppCompatActivity(), OnMapReadyCallback {
+
+class MapsActivity: AppCompatActivity(), OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: MapsLokasiBinding
     private lateinit var mMap: GoogleMap
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<String>,
-        grantResults: IntArray
+        grantResults: IntArray,
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
@@ -59,12 +64,72 @@ class MapsActivity: AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map_rute) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        binding.apply {
+            navigationFilter.setNavigationItemSelectedListener(this@MapsActivity)
+
+            buttonBack.setOnClickListener { moveToHome() }
+            buttonFilter.setOnClickListener{
+                if(drawerLayout.isDrawerOpen(GravityCompat.END)){
+                    drawerLayout.closeDrawer(GravityCompat.END)
+                }
+                drawerLayout.openDrawer(GravityCompat.END)
+            }
+            buttonSetting.setOnClickListener { moveToSetting() }
+            buttonMyLocation.setOnClickListener { getMyLocation() }
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+
+        mMap.uiSettings.isIndoorLevelPickerEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.uiSettings.isMapToolbarEnabled = true
+
+        val kotaMalang = LatLng(-7.982929, 112.631333)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(kotaMalang, 13.5f))
+
+        getMyLocation()
+
     }
 
+        private fun moveToHome(){
+        val intent = Intent(this@MapsActivity, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun moveToSetting(){
+        val intent = Intent(this@MapsActivity, SettingActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                getMyLocation()
+            }
+        }
+
+    private fun getMyLocation() {
+        if (ContextCompat.checkSelfPermission(
+                this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.uiSettings.isMyLocationButtonEnabled = true
+        } else {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
 
     companion object{
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
