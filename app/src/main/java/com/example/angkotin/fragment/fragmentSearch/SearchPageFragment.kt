847164bc707b92d190d983f2e5d575e37d141705
anggtpd <anggitapinasthikadewi@gmail.com>
2022-06-10 18:06:07 +0700

    menambahkan fitur alert dialog utk profil driver

--- app/src/main/java/com/example/angkotin/fragment/fragmentSearch/SearchPageFragment.kt
+++ app/src/main/java/com/example/angkotin/fragment/fragmentSearch/SearchPageFragment.kt
@@ -5,12 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.angkotin.R
+import com.example.angkotin.databinding.FragmentSearchPageBinding
 
 class SearchPageFragment : Fragment() {
+    private var _binding: FragmentSearchPageBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_page, container, false)
+        _binding = FragmentSearchPageBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonBack.setOnClickListener{
+            view?.findNavController()?.navigate(R.id.action_searchPageFragment_to_mapFragment)
+        }
     }
 }
